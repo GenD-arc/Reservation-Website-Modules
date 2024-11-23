861336ec43cf3625d27b26852c8ffacbcfbbed75
Giiin <zerepgen@gmail.com>
2024-11-24 04:00:57 +0800

    Add files via upload

---
+++ mywebsite/components/footer.php
@@ -0,0 +1,29 @@
+<!-- footer section starts  -->
+
+<section class="footer" id="footer">
+    <div class="box-container">
+        <div class="box">
+            <a href="tel:0123456789"><i class="fas fa-phone"></i>09186921661</a>
+            <a href="mailto:casacelerinafarmresort@gmail.com"><i class="fas fa-envelope"></i>casacelerinafarmresort@gmail.com</a>
+            <a href="#"><i class="fas fa-map-marker-alt"></i>Kanlurang Robles, Barangay Masalukot 2 4323 Candelaria, Philippines</a>
+        </div>
+        <div class="box">
+        <a href="#home">Home</a>
+         <a href="#about">About</a>
+         <a href="bookings.php">My Bookings</a>
+         <a href="#reservation">Reservation</a>
+         <a href="#gallery">Gallery</a>
+         <a href="#contact">Contact</a>
+         <a href="#reviews">Reviews</a>
+        </div>
+        <div class="box">
+            <a href="#"> Facebook <i class="fab fa-facebook-f"></i></a>
+            <a href="#"> Instagram <i class="fab fa-instagram"></i></a>
+        </div>
+    </div>
+
+<div class="credit"> &copy; copyright @ 2024 by cs students | mwamwa </div>
+
+</section>
+
+<!-- footer section ends -->
